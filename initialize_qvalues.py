a032f55d3da0a25ae0f1a7087cde3bef7ff0f611
abelespinromero <abelespinromero@gmail.com>
2023-10-10 23:55:34 +0200

    fix bugs

--- initialize_qvalues.py
+++ initialize_qvalues.py
@@ -1,12 +1,14 @@
 
 import json
 
+width, height = 400, 300
+
 def initialize_q_values():
     q_values = {}
     actions = ["UP", "DOWN", "LEFT", "RIGHT"]
     
-    for x in range(0, 801, 10):
-        for y in range(0, 601, 10):
+    for x in range(0, width+1, 10):
+        for y in range(0, height+1, 10):
             state = ((x, y),)
             q_values[str(state)] = {action: 0 for action in actions}
 
